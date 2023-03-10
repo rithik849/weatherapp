184b587b0ef3d9bbbdf67e79c50f54cab36529fc
Rithik Patel <rithiknpatel@gmail.com>
2023-03-10 22:14:24 +0000

    Add project wide constants

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+import {URL_Type} from "./project_types/types"
+
+const url : URL_Type = "https://api.open-meteo.com/v1/forecast?"+
+"latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min&timezone=Europe%2FLondon";
+
+export {url}
