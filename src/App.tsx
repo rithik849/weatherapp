e03d83a8f6536884c719721fb92b28c7f010fdb9
Rithik Patel <rithiknpatel@gmail.com>
2023-03-28 16:16:41 +0100

    Add Custom Types

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 
-import './App.css'
-import { URL_Type } from './project_types/types'
+import './styles/App.css'
+import { URL_Type, IFetchedData, WeatherProps } from './project_types/types'
 import {url} from './constants.js'
+import {WeatherItem} from "./WeatherItem"
+
 
 
 const App : React.FC = () => {
-    const [fetchedData, setFetchedData] = useState<JSON | null>(null);
+    const [fetchedData, setFetchedData] = useState<IFetchedData | null>(null);
 
     useEffect( () => {
         const controller = new AbortController();
         const abortSignal = controller.signal;
 
-        async function getWeather(url : URL_Type, abortSignal : AbortSignal){
+        async function getWeather(url : URL_Type, abortSignal : AbortSignal) : Promise<void>{
             try{
         
                 const res : Response = await fetch(url,{method : 'get',signal : abortSignal});
-                const resJSON : JSON = (await res.json())["daily"];
+                const resJSON : IFetchedData = (await res.json())["daily"];
                 setFetchedData((resJSON))
             }catch(e){
                 if (! controller.signal.aborted){
@@ -32,11 +34,17 @@ const App : React.FC = () => {
 
     },[])
 
-    console.log(url);
+
+
 
     return (
     <div className="App">
-        <p>{JSON.stringify(fetchedData)}</p>
+        {
+            fetchedData?.time.map(
+                (item,index) =>
+                    <WeatherItem date= {item} min_temp= {fetchedData.temperature_2m_min[index]} max_temp = {fetchedData.temperature_2m_max[index]} />
+            )
+        }
     </div>
     )
 };
--- src/project_types/types.d.ts
+++ src/project_types/types.d.ts
@@ -1,1 +1,14 @@
 export type URL_Type = string;
+
+export interface IFetchedData {
+    time : string[];
+    temperature_2m_min : number[];
+    temperature_2m_max : number[];
+}
+
+
+export interface WeatherProps { 
+    date : string,
+    min_temp : number,
+    max_temp : number
+}
