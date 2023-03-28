2a02befe245db671238790469e26fd4b717495fe
Rithik Patel <rithiknpatel@gmail.com>
2023-03-28 18:56:27 +0100

    Add Weather Item Component

---
+++ src/WeatherItem.tsx
@@ -0,0 +1,32 @@
+import './styles/WeatherItem.css'
+import {WeatherProps} from "./project_types/types"
+import { useEffect, useMemo, useRef, useState } from 'react';
+
+const WeatherItem :React.FC<React.PropsWithChildren<WeatherProps>> = (prop) : React.ReactElement => {
+    const [date,setDate]  =  useState<Date>(new Date())
+
+    useEffect(() => {
+        const date : {year : number, month : number , day : number }= {
+            year: parseInt(prop.date.substring(0,4),10),
+            month: parseInt(prop.date.substring(5,7),10) - 1,
+            day: parseInt(prop.date.substring(8,10),10)
+        };
+        console.log(date)
+        setDate(new Date(date.year,date.month,date.day))
+    },[])
+
+
+    return (
+            <div className = "WeatherItem" onClick={()=>console.log("Clicked!")}>
+                <div className='Contents'>
+                    <div className="Day">{date.toDateString().substring(0,4)} </div>
+                    <div className="Date">{prop.date}  </div>
+                    <div className="Min_temp">{prop.min_temp + "°C"}</div> 
+                    <div className="Max_temp">{prop.max_temp + "°C"}</div>
+                </div>
+            </div>
+        )
+
+}
+
+export {WeatherItem}
