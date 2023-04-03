0716b40d70a55ed2525248f85f237354eb3b25bc
Rithik Patel <rithiknpatel@gmail.com>
2023-04-03 16:45:39 +0100

    Add routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,20 @@
 import { Forecast } from './Forecast'
-
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
 
 
 const App : React.FC = () => {
 
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route>
+            <Route path="/" element = {<Forecast/>} />
+            <Route path="*" element = {<h1>Err</h1>} />
+            </Route>
+    ))
+
 
     return (
-        <Forecast/>
+        <RouterProvider router = {router}/>
     )
 };
 
