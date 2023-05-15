f4851f63e686a040de568650c76dc112bab66740
Rithik Patel <rithiknpatel@gmail.com>
2023-05-15 21:37:03 +0100

    Add plot of line

--- src/components/WeatherChart.tsx
+++ src/components/WeatherChart.tsx
@@ -38,12 +38,10 @@ class WeatherChart extends React.Component<GraphProps,GraphState> {
         if (this.state.points.length === 0){
             throw new Error('No data to plot!')
         }
+
         console.log(this.state)
         const margin = {top: 10, right: 30, bottom: 50, left: 60}
         const dimensions = {width : 1080 - margin.left - margin.right , height : 300 - margin.top - margin.bottom}
-        
-        // const x = d3.scaleBand().range([0, dimensions.width]).padding(0.1);
-        // const y = d3.scaleLinear().domain([0,50]).range([dimensions.height, 0]);
 
         const svg = d3.select(".graph")
         .append("svg")
@@ -68,17 +66,19 @@ class WeatherChart extends React.Component<GraphProps,GraphState> {
 
         const xAxis : d3.ScaleTime<number,number,never> = d3.scaleTime().domain(minmaxDate).rangeRound([0,dimensions.width])
 
-        const yAxis = d3.scaleLinear().domain(minmaxTemperature).rangeRound([dimensions.height,0])
+        const yAxis : d3.ScaleLinear<number,number> = d3.scaleLinear().domain(minmaxTemperature).rangeRound([dimensions.height,0])
 
         // Plot x axis
-        svg.append("g").attr("color","black").attr("transform","translate("+margin.left+","+(margin.top+dimensions.height)+")").call(
+        svg.append("g")
+        .attr("color","black")
+        .attr("transform","translate("+margin.left+","+(margin.top+dimensions.height)+")")
+        .call(
             d3.axisBottom(xAxis).tickFormat(
                 (d) => d3.timeFormat('%H:%M')(d as Date)
-            ).ticks(24)
-        )
+                ).ticks(24)
+            )
 
         svg.append("text")
-        
         .attr("class","x-label")
         .attr("x",margin.left + dimensions.width/2)
         .attr("y",margin.top+dimensions.height + (margin.bottom*3/4))
@@ -87,9 +87,12 @@ class WeatherChart extends React.Component<GraphProps,GraphState> {
         
 
         // Plot y axis
-        svg.append("g").attr("color","black").attr("transform","translate("+margin.left+","+margin.top+")").call(
+        svg.append("g")
+        .attr("color","black")
+        .attr("transform","translate("+margin.left+","+margin.top+")")
+        .call(
             d3.axisLeft(yAxis)
-        )
+            )
 
         svg.append("text")
         .attr("class", "y-label")
@@ -98,6 +101,22 @@ class WeatherChart extends React.Component<GraphProps,GraphState> {
         .attr("y", margin.left/3)
         .style("text-anchor", "middle")
         .text(this.state.y_label)
+
+
+        // Plot path
+
+        svg.append("path")
+        .datum(this.state.points)
+        .attr("id","line")
+        .attr("fill", "none")
+        .attr("transform","translate("+margin.left+","+margin.top+")")
+        .attr("stroke", "black")
+        .attr("stroke-width", 1.5)
+        .attr("d", d3.line(
+            d => {return xAxis(d[0])}, d => {return yAxis(d[1])}
+            )
+        )
+    
         
     }
 
